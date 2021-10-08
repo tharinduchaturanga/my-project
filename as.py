9179537bbab1057112346a18b47994074f30e5c1
tharinduchaturanga <92161995+tharinduchaturanga@users.noreply.github.com>
2021-10-08 18:55:01 +0530

    Add files via upload

---
+++ as.py
@@ -0,0 +1,6 @@
+Python 2.7 (r27:82525, Jul  4 2010, 07:43:08) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 5
+while x > 0:
+    print 'X :', x
+    x = x - 1
